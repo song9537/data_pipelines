772500fbb5e0af9a65aef8a53335f75c55094f19
dn757657 <daniel.js.campbell@gmail.com>
2023-07-28 14:14:30 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,10 +6,11 @@ with open("./requirements.txt") as requirement_file:
 setup(
     name="data_pipelines",
     description="etl pipelines, various sources",
-    version="1.0.0",
+    version="1.0.1",
     author="dn757657",
     author_email="dn757657@dal.ca",
     install_requires=requirements,
     packages=find_packages(include=['dn757657_crypto_num_sources',
-                                    'dn757657_data_endpoints']),  # package = any folder with an __init__.py file
+                                    'dn757657_data_endpoints'],
+                           exclude=['dn757657_fin_news_sources']),  # package = any folder with an __init__.py file
 )
